3e747166585d45032d348fb80988c269adc1e436
tMarvin91 <t-marvin@web.de>
2018-06-12 17:37:03 +0200

    first commit

---
+++ src/main/java/com/muffdi/kafka/StreamsStarterApp.java
@@ -0,0 +1,20 @@
+package com.muffdi.kafka;
+
+import kafka.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
+public class StreamsStarterApp {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-counts");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.AutoOffsetReset(), "earliest");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    }
+}
